a0142764d3893b471a8e771efba03a37f1e26893
Kukhianidze <KUKH004@dx1.admgt.net>
2022-11-28 13:31:20 +0400

    add basic category component ts

--- src/app/views/categories-pg/categories-pg.component.ts
+++ src/app/views/categories-pg/categories-pg.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/shared/shared-services/categories.service';
 
 @Component({
   selector: 'app-categories-pg',
   templateUrl: './categories-pg.component.html',
-  styleUrls: ['./categories-pg.component.css']
+  styleUrls: ['./categories-pg.component.css'],
 })
 export class CategoriesPgComponent implements OnInit {
+  categories: any[] = [];
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService) {}
 
   ngOnInit(): void {
+    this.readAllCategories();
   }
 
+  readAllCategories() {
+    this.categoriesService.readAllCategories().subscribe((response: any) => {
+      this.categories = response.data;
+      console.log(this.categories);
+    });
+  }
 }
